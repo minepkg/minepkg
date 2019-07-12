1326636dbc931ba78cdaf98ec07c3f7ca7bc7333
Filip Weiss <me@fiws.net>
2019-07-12 18:55:50 +0200

    actually fix linux selfupdate

--- assets/linux-installer.sh
+++ assets/linux-installer.sh
@@ -1,4 +1,7 @@
 #!bin/sh
 
 echo "installing minepkg"
-sudo curl -L "https://storage.googleapis.com/minepkg-client/latest/minepkg-linux-amd64" -o /usr/local/bin/minepkg && sudo chmod +x /usr/local/bin/minepkg && sudo chown $USER /usr/local/bin/minepkg
+mkdir -p $HOME/.minepkg
+curl -L "https://storage.googleapis.com/minepkg-client/latest/minepkg-linux-amd64" -o $HOME/.minepkg/minepkg
+chmod +x $HOME/.minepkg/minepkg
+sudo ln -fs $HOME/.minepkg/minepkg /usr/local/bin/minepkg
