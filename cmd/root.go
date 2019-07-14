665b504abf36896ff0a4e263a6cbf73175a806f4
Filip Weiss <me@fiws.net>
2019-07-14 15:01:30 +0200

    0.0.13

--- cmd/root.go
+++ cmd/root.go
@@ -19,7 +19,7 @@ import (
 )
 
 // MinepkgVersion is a constant of the current minepkg version
-const MinepkgVersion = "0.0.12"
+const MinepkgVersion = "0.0.13"
 
 var cfgFile string
 var logger *cmdlog.Logger = cmdlog.New()
