b1fdd915d09189e8a5af21e7c994e64c414318ed
Filip Weiss <me@fiws.net>
2019-08-17 19:00:26 +0200

    0.0.16

--- cmd/root.go
+++ cmd/root.go
@@ -19,7 +19,7 @@ import (
 )
 
 // MinepkgVersion is a constant of the current minepkg version
-const MinepkgVersion = "0.0.15"
+const MinepkgVersion = "0.0.16"
 
 // TODO: this logger is not so great – also: it should not be global
 var logger *cmdlog.Logger = cmdlog.New()
