e27a02351b6c68abc583535f27642b24fed05b77
Filip Weiss <me@fiws.net>
2021-05-25 11:41:51 +0200

    query releases: properly output errors

--- internals/api/query-release.go
+++ internals/api/query-release.go
@@ -2,10 +2,39 @@ package api
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"net/http"
 	"net/url"
 
 	"github.com/Masterminds/semver/v3"
 )
+
+// ErrNoMatchingRelease is returned if a wanted package query could not be resolved
+type ErrNoQueryResult struct {
+	// Query is the query that did not resolve
+	Query *ReleasesQuery
+	// Err is the underlying error that describes why there was no package found
+	Err error
+}
+
+func (e *ErrNoQueryResult) Error() string {
+	if e.Query == nil {
+		return e.Error()
+	}
+	minecraft := e.Query.Minecraft
+	if e.Query.Minecraft == "" {
+		minecraft = "*"
+	}
+	return fmt.Sprintf(
+		"query for name: %s@%s (platform: %s, minecraft: %s) failed: %s",
+		e.Query.Name,
+		e.Query.VersionRange,
+		e.Query.Platform,
+		minecraft,
+		e.Err.Error(),
+	)
+}
 
 // ReleasesQuery is a query to find a release
 type ReleasesQuery struct {
@@ -42,28 +71,35 @@ func (m *MinepkgAPI) ReleasesQuery(ctx context.Context, query *ReleasesQuery) (*
 	}
 	defer res.Body.Close()
 
-	// TODO: return more specific errors here
-	if err := checkResponse(res); err != nil {
-		if err == ErrNotFound {
-			e, ok := err.(MinepkgError)
-			if !ok {
-				return nil, err
-			}
-			switch e.ResolveError {
-			case "minecraft_req_not_satisfiable":
-				return nil, ErrNoReleaseForMinecraftVersion
-			case "version_req_not_satisfiable":
-				return nil, ErrNoReleaseForVersion
-			case "project_does_not_exist":
-				return nil, ErrProjectDoesNotExist
-			case "no_releases_for_platform":
-				return nil, ErrNoReleasesForPlatform
-			case "all_reqs_not_satisfiable":
-				return nil, ErrNoReleaseWithConstrains
-			default:
-				return nil, ErrNoReleaseWithConstrains
-			}
+	// special handle for 404 errors
+	if res.StatusCode == http.StatusNotFound {
+		minepkgErr := &MinepkgError{}
+		if err := parseJSON(res, minepkgErr); err != nil {
+			return nil, errors.New("minepkg API did respond with expected error format. code: " + res.Status)
 		}
+
+		resolveErr := &ErrNoQueryResult{query, nil}
+		switch minepkgErr.ResolveError {
+		case "minecraft_req_not_satisfiable":
+			resolveErr.Err = ErrNoReleaseForMinecraftVersion
+		case "version_req_not_satisfiable":
+			resolveErr.Err = ErrNoReleaseForVersion
+		case "project_does_not_exist":
+			resolveErr.Err = ErrProjectDoesNotExist
+		case "no_releases_for_platform":
+			resolveErr.Err = ErrNoReleasesForPlatform
+		case "all_reqs_not_satisfiable":
+			resolveErr.Err = ErrNoReleaseWithConstrains
+		default:
+			// unknown error
+			return nil, err
+		}
+		// return enriched resolve error
+		return nil, resolveErr
+	}
+
+	// all other errors
+	if err := checkResponse(res); err != nil {
 		return nil, err
 	}
 
