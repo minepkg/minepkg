f4524db54a5f2d38154ae46f0357d5413d3c0346
Filip Weiss <me@fiws.net>
2021-07-08 01:14:42 +0200

    java: remove unused adopt api function

--- internals/java/adopt.go
+++ internals/java/adopt.go
@@ -13,30 +13,16 @@ import (
 
 const AdoptAPI = "https://api.adoptopenjdk.net/v3"
 
-type AdoptReleaseNamesRequest struct {
-	architecture string
-	heapSize     string
-	imageType    string
-	jvmImpl      string
-	lts          bool
-	os           string
-	version      string
-}
-
 type AdoptAssetRequest struct {
 	featureVersion uint16
 	releaseType    string
 	architecture   string
-	before         string
-	heapSize       string
-	imageType      string
-	jvmImpl        string
-	os             string
-	vendor         string
-}
-
-type AdoptReleaseNames struct {
-	Releases []string `json:"releases"`
+	// before         string
+	heapSize  string
+	imageType string
+	jvmImpl   string
+	os        string
+	vendor    string
 }
 
 type AdoptAsset struct {
@@ -76,22 +62,6 @@ type AdoptAsset struct {
 		Security       int    `json:"security"`
 		Semver         string `json:"semver"`
 	} `json:"version_data"`
-}
-
-func (j *Factory) getLatest(ctx context.Context) (*AdoptReleaseNames, error) {
-	opts := AdoptReleaseNamesRequest{
-		lts: false,
-	}
-
-	return j.getReleaseNames(ctx, &opts)
-}
-
-func (j *Factory) getLatestLTS(ctx context.Context) (*AdoptReleaseNames, error) {
-	opts := AdoptReleaseNamesRequest{
-		lts: true,
-	}
-
-	return j.getReleaseNames(ctx, &opts)
 }
 
 func (j *Factory) getAssets(ctx context.Context, opts *AdoptAssetRequest) ([]AdoptAsset, error) {
@@ -173,67 +143,6 @@ func (j *Factory) getAssets(ctx context.Context, opts *AdoptAssetRequest) ([]Ado
 	}
 
 	return parsed, nil
-}
-
-func (j *Factory) getReleaseNames(ctx context.Context, opts *AdoptReleaseNamesRequest) (*AdoptReleaseNames, error) {
-	if opts.architecture == "" {
-		opts.architecture = archMap(runtime.GOARCH)
-	}
-	if opts.os == "" {
-		osName := runtime.GOOS
-		if osName == "darwin" {
-			osName = "mac"
-		}
-
-		// i wanna see if this ever happens
-		if osName == "android" {
-			osName = "linux"
-		}
-
-		// we check for alpine if os is linux, as it needs a different jdk
-		if osName == "linux" {
-			if _, err := os.Stat("/etc/alpine-release"); !os.IsNotExist(err) {
-				osName = "alpine-linux"
-			}
-		}
-		opts.os = osName
-	}
-	if opts.jvmImpl == "" {
-		opts.jvmImpl = "openj9"
-	}
-	params := url.Values{}
-	params.Add("architecture", opts.architecture)
-	if opts.heapSize != "" {
-		params.Add("heap_size", opts.heapSize)
-	}
-	if opts.imageType != "" {
-		params.Add("image_type", opts.imageType)
-	}
-	if opts.version != "" {
-		params.Add("version", opts.version)
-	}
-	params.Add("jvm_impl", opts.jvmImpl)
-	params.Add("lts", fmt.Sprint(opts.lts))
-	params.Add("os", opts.os)
-
-	req, err := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("%s/info/release_names?%s", AdoptAPI, params.Encode()), nil)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("Accept", "application/json")
-	req.Header.Set("Content-Type", "application/json")
-
-	res, err := j.http.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	parsed := AdoptReleaseNames{}
-	if err = json.NewDecoder(res.Body).Decode(&parsed); err != nil {
-		return nil, err
-	}
-
-	return &parsed, nil
 }
 
 func archMap(arch string) string {
