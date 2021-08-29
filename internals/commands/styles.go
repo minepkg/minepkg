3bc85a8f95343e481120bff1186cb88a31a1d94b
Filip Weiss <me@fiws.net>
2021-08-29 16:42:46 +0200

    simpler error messages (avoid many terminal problems)

--- internals/commands/styles.go
+++ internals/commands/styles.go
@@ -7,18 +7,14 @@ import (
 )
 
 var styleErrBox = lipgloss.NewStyle().
-	Width(80).
 	MarginTop(1).
 	Bold(true).
-	Background(lipgloss.AdaptiveColor{Light: "#ffcdd2", Dark: "#512222"}).
 	Foreground(lipgloss.AdaptiveColor{Light: "#b71c1c", Dark: "#fa8a8a"}).
 	Border(lipgloss.NormalBorder(), false, false, false, true).
 	BorderLeftForeground(lipgloss.Color("#f86262")).
-	Padding(1, 2)
+	Padding(0, 1)
 
 var styleHelpBox = lipgloss.NewStyle().
-	Width(80).
-	Background(lipgloss.AdaptiveColor{Light: "#e9e9e9", Dark: "#2f2f2f"}).
 	Padding(0, 2).
 	Margin(0, 1).
 	PaddingTop(1)
@@ -43,25 +39,23 @@ var warnBorder = lipgloss.Border{
 var StyleWarnBox = lipgloss.NewStyle().
 	MarginTop(1).
 	Bold(true).
-	Background(lipgloss.AdaptiveColor{Light: "#ffe09d", Dark: "#5b4729"}).
 	Foreground(lipgloss.AdaptiveColor{Light: "#774d00", Dark: "#ffe5d5"}).
 	Border(lipgloss.NormalBorder(), false, false, false, true).
 	BorderLeftForeground(lipgloss.Color("#ffb837")).
-	Padding(1, 2)
+	Padding(0, 1)
 
 var StyleInfoBox = lipgloss.NewStyle().
 	MarginTop(1).
 	Bold(true).
-	Background(lipgloss.AdaptiveColor{Light: "#a9d0ff", Dark: "#263040"}).
 	Foreground(lipgloss.AdaptiveColor{Light: "#00174c", Dark: "#e7d5ff"}).
 	Border(lipgloss.NormalBorder(), false, false, false, true).
 	BorderLeftForeground(lipgloss.Color("#5e5eff")).
-	Padding(1, 2)
+	Padding(1, 1)
 
 func ErrorBox(errorString string, helpText string) string {
 	rendered := styleErrBox.Render(
 		lipgloss.JoinHorizontal(
-			lipgloss.Top, Emoji("❗ "),
+			lipgloss.Top, Emoji("💥 "),
 			styleErrText.Render(fmt.Sprintf("Error: %s", errorString)),
 		),
 	)
