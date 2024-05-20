1e1c88956882911e153a7d65ba2edec49a1bde27
Filip Weiss <me@fiws.net>
2024-05-20 11:02:45 +0200

    mostly dependency updates

--- go.mod
+++ go.mod
@@ -35,12 +35,10 @@ require (
 	github.com/alessio/shellescape v1.4.2 // indirect
 	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/atotto/clipboard v0.1.4 // indirect
-	github.com/aymanbagabas/go-osc52 v1.2.2 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/bep/debounce v1.2.1 // indirect
 	github.com/charmbracelet/harmonica v0.2.0 // indirect
 	github.com/chzyer/readline v1.5.1 // indirect
-	github.com/containerd/console v1.0.4 // indirect
 	github.com/danieljoos/wincred v1.2.1 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/dsnet/compress v0.0.2-0.20210315054119-f66993602bf5 // indirect
@@ -49,7 +47,6 @@ require (
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
-	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
@@ -58,8 +55,6 @@ require (
 	github.com/jwalton/go-supportscolor v1.2.0 // indirect
 	github.com/klauspost/compress v1.17.8 // indirect
 	github.com/klauspost/pgzip v1.2.6 // indirect
-	github.com/kr/pretty v0.3.1 // indirect
-	github.com/kr/text v0.2.0 // indirect
 	github.com/labstack/echo/v4 v4.12.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/leaanthony/go-ansi-parser v1.6.1 // indirect
@@ -94,12 +89,10 @@ require (
 	github.com/pion/stun v0.6.1 // indirect
 	github.com/pion/transport/v2 v2.2.5 // indirect
 	github.com/pion/turn/v2 v2.1.6 // indirect
-	github.com/pion/udp/v2 v2.0.1 // indirect
 	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/power-devops/perfstat v0.0.0-20240221224432-82ca36839d55 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
-	github.com/rogpeppe/go-internal v1.9.0 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/samber/lo v1.39.0 // indirect
@@ -107,7 +100,6 @@ require (
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
-	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
@@ -121,7 +113,6 @@ require (
 	github.com/wailsapp/mimetype v1.4.1 // indirect
 	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
-	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
@@ -129,8 +120,6 @@ require (
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/term v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
-	google.golang.org/appengine v1.6.8 // indirect
-	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 )
--- gui/frontend/src/App.tsx
+++ gui/frontend/src/App.tsx
@@ -1,43 +1,66 @@
-import {useState} from 'react';
-import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import { useState } from "react";
+import "./App.css";
+import { Greet } from "../wailsjs/go/main/App";
 
-import { Quit } from '../wailsjs/runtime/runtime'
+import { Quit } from "../wailsjs/runtime/runtime";
+import { MinusIcon, Square, XIcon } from "lucide-react";
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
+  const [resultText, setResultText] = useState(
+    "Please enter your name below 👇"
+  );
+  const [name, setName] = useState("");
+  const updateName = (e: any) => setName(e.target.value);
 
-    function joinServer() {
-        console.log('Joining server: ' + name);
-        Greet(name);
-    }
+  function joinServer() {
+    console.log("Joining server: " + name);
+    Greet(name);
+  }
 
-    return (
-        <div id="app" className='bg-zinc-900 border-zinc-700 border'>
-            <nav className="draggable flex px-2 justify-between items-center py-2 bg-zinc-800 shadow-lg">
-                <div>
-                    minepkg
-                </div>
-                <div className='flex gap-2'>
-                    <div className='p-2 px-4 cursor-default rounded hover:bg-white/10' onClick={Quit}>
-                        -
-                    </div>
-                    <div className='p-2 px-4 cursor-default rounded hover:bg-white/10' onClick={Quit}>
-                        O
-                    </div>
-                    <div className='p-2 px-4 cursor-default rounded hover:bg-white/10' onClick={Quit}>
-                        x
-                    </div>
-                </div>
-            </nav>
-            <div id="input" className="flex gap-2 justify-center p-8">
-                <input id="name" className="p-2 rounded-lg shadow bg-zinc-800 text-white" placeholder="Server IP Address" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="p-2 px-4 bg-orange-600 shadow-orange-600/30 shadow-lg font-bold rounded-lg" onClick={joinServer}>Join Server</button>
-            </div>
+  return (
+    <div id="app" className="bg-zinc-900 border-zinc-700 border">
+      <nav className="draggable flex px-2 justify-between items-center py-2 bg-zinc-800 shadow-lg">
+        <div>minepkg</div>
+        <div className="flex gap-2">
+          <div
+            className="p-2 px-4 cursor-default rounded hover:bg-white/10"
+            onClick={Quit}
+          >
+            <MinusIcon className="w-4" />
+          </div>
+          <div
+            className="p-2 px-4 cursor-default rounded hover:bg-white/10"
+            onClick={Quit}
+          >
+            <Square className="w-4" />
+          </div>
+          <div
+            className="p-2 px-4 cursor-default rounded hover:bg-white/10"
+            onClick={Quit}
+          >
+            <XIcon className="w-4" onClick={Quit} />
+          </div>
         </div>
-    )
+      </nav>
+      <div id="input" className="flex gap-2 justify-center p-8">
+        <input
+          id="name"
+          className="p-2 rounded-lg shadow bg-zinc-800 text-white"
+          placeholder="Server IP Address"
+          onChange={updateName}
+          autoComplete="off"
+          name="input"
+          type="text"
+        />
+        <button
+          className="p-2 px-4 bg-orange-600 shadow-orange-600/30 shadow-lg font-bold rounded-lg"
+          onClick={joinServer}
+        >
+          Join Server
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
